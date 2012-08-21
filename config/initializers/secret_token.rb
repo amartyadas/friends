d2a5b5b76131579beea1dff72c885fa5cbc4c27c
Amartya <amartya@amartya-laptop.(none)>
2012-08-21 14:43:31 +0530

    Amartya_test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friends::Application.config.secret_token = '905a2a1243e5ff61923a785ebf98ddbb6307446e354c305d7e8a3246f7c613f9cf723ff7dae2885ed02cdc3ec37f0c6fcc3ddb3a731e506116525496e2453b23'
